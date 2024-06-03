5e7e37774268f621e5a5392054ccbacc9ae8abd6
Deborah <debbiendole@gmail.com>
2024-06-04 02:27:11 +0300

    done

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,24 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def _init_(self, name= "Unknown", breed="Mutt"):
+        self.name = name
+        self.breed = breed
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value  
+        else:
+            print("Name must be string between 1 and 25 characters.")
+   
+    @property
+    def breed(self):
+        return self._breed
+
+    @breed.setter
+    def breed(self, value):
+        self._breed = value
